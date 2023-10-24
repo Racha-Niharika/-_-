9936e520b8b93bfaf3a0ac757e017d2ec2056321
Racha-Niharika <148681715+Racha-Niharika@users.noreply.github.com>
2023-10-24 18:28:14 +0530

    Update script.js

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,1 +1,37 @@
+// DOM Elements
+const taskInput = document.getElementById('taskInput');
+const addTaskBtn = document.getElementById('addTaskBtn');
+const taskList = document.getElementById('taskList');
+
+// Add a new task
+addTaskBtn.addEventListener('click', () => {
+    const taskText = taskInput.value.trim();
+    if (taskText !== '') {
+        createTask(taskText);
+        taskInput.value = '';
+    }
+});
+
+// Create a new task
+function createTask(taskText) {
+    const li = document.createElement('li');
+    li.innerHTML = `
+        <span>${taskText}</span>
+        <button class="delete-btn">Delete</button>
+    `;
+    taskList.appendChild(li);
+
+    // Add event listener to delete button
+    const deleteBtn = li.querySelector('.delete-btn');
+    deleteBtn.addEventListener('click', () => {
+        taskList.removeChild(li);
+    });
+}
+
+// Handle pressing Enter key to add a task
+taskInput.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        addTaskBtn.click();
+    }
+});
 

7fd4393a732c2cfa39c4359d967fe2d8824a149d
Racha-Niharika <148681715+Racha-Niharika@users.noreply.github.com>
2023-10-24 18:32:26 +0530

    Update script.js

--- age-calculator/script.js
+++ age-calculator/script.js
@@ -1,1 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const calculateButton = document.getElementById("calculateButton");
+    const resultElement = document.getElementById("result");
+
+    calculateButton.addEventListener("click", calculateAge);
+
+    function calculateAge() {
+        const day = parseInt(document.getElementById("dob").value);
+        const month = parseInt(document.getElementById("month").value) - 1; // JavaScript months are 0-based
+        const year = parseInt(document.getElementById("year").value);
+        
+        if (isNaN(day) || isNaN(month) || isNaN(year)) {
+            resultElement.textContent = "Please enter a valid date of birth.";
+            return;
+        }
+
+        const currentDate = new Date();
+        const dob = new Date(year, month, day);
+        const ageInMilliseconds = currentDate - dob;
+        const ageInYears = ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25);
+        
+        if (ageInYears < 0) {
+            resultElement.textContent = "Birthdate is in the future. Please enter a valid date.";
+        } else {
+            resultElement.textContent = `Your age is approximately ${ageInYears.toFixed(2)} years.`;
+        }
+    }
+});
 
